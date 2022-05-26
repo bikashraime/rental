d9b8c98e46239d5aadbb385b709129e1aaf683c2
paras <parasrai@Jarvis.local>
2022-05-26 19:23:21 +0545

    Footer completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import { Col, Container, Row } from 'react-bootstrap'
 import logo from '../assets/images/logo.png';
 import email from '../assets/icons/email.png';
 import phone from '../assets/icons/phone.png';
+import location from '../assets/icons/locationpoint.png';
 // import phone from '../Images/phone.svg'
 // import email from '../Images/email.svg'
 // import companyLogo from './Footer images/companyLogo.png'
@@ -10,30 +11,39 @@ import phone from '../assets/icons/phone.png';
 function Footer() {
     return (
         <div className='py-3' style={{ backgroundColor: "#313539" }} >
-
             <Container style={{ paddingTop: "30px", paddingBottom: "30px" }} >
-                <div className='d-flex'>
-                    <Row>
-                        <Col className='d-flex' sm={6} md={3} xs={12}>
-                            <img className='align-self-sm-center' src={logo} alt="logo" style={{ width: "100%" }} style={{ maxWidth: 200 }} />
-                        </Col>
-                        <Col className="text-white" sm={9} md={8} xs={12}>
-                            <Row>
-                                <Col>
-                                    <div className='px-3 mx-3'>
-                                        <span className=''>QUICK LINK</span>
-                                        <a href=""><span>Home</span></a>
-                                        <a href=""><span>Categories</span></a>
-                                        <a href=""><span>About us</span></a>
-                                        <a href=""><span>Contact us</span></a>
+                {/* <div className='d-flex'> */}
+                <Row>
+                    <Col className='d-flex justify-content-center' sm={6} md={4} xs={12} style={{ marginBottom: 30 }}>
+                        <img className='align-self-md-center' src={logo} alt="logo" style={{ maxWidth: 200 }} />
+                    </Col>
+                    <Col className="text-white" sm={9} md={8} xs={12}>
+                        <Row>
+                            <Col md={6} sm={12} xs={12}>
+                                <div className='px-3 mx-3  justify-content-center' style={{ marginBottom: 30 }}>
+                                    <div className='footer-title'>QUICK LINK</div>
+                                    <div className='footer-page-link'>
+                                        <li className='bottom-nab'>
+                                            <a href=""><span>Home</span></a>
+                                        </li>
+                                        <li className='bottom-nab'>
+                                            <a href=""><span>Categories</span></a>
+                                        </li>
+                                        <li className='bottom-nab'>
+                                            <a href=""><span>About us</span></a>
+                                        </li>
+                                        <li className='bottom-nab'>
+                                            <a href=""><span>Contact us</span></a>
+                                        </li>
                                     </div>
 
-                                </Col>
-
-                                <Col>
-                                    <div className='px-3 mx-3'>
-                                        <div className=''>CONTACT</div>
-                                        <div className=''>
+                                </div>
+                            </Col>
+                            <Col md={6} sm={12} xs={12}>
+                                <div className='px-3 mx-3  justify-content-center'>
+                                    <div className='footer-title'>CONTACT</div>
+                                    <div className='footer-page-link '>
+                                        <div className='my-1'>
                                             <img src={phone} width={20} />
                                             <span className='mx-2'>1234567890</span>
                                         </div>
@@ -41,18 +51,21 @@ function Footer() {
                                             <img src={email} width={20} color="#fff" />
                                             <span className='mx-2'>abcdef@gmail.com</span>
                                         </div>
-                                        <div>
-                                            <span>Biratnagar, Nepal</span>
+
+                                        <div className='my-1'>
+                                            <img src={location} width={20} color="#fff" />
+                                            <span className='mx-2'>Biratnagar, Nepal</span>
                                         </div>
                                     </div>
+                                </div>
 
-                                </Col>
+                            </Col>
 
-                            </Row>
-                        </Col>
+                        </Row>
+                    </Col>
 
-                    </Row>
-                </div>
+                </Row>
+                {/* </div> */}
             </Container>
         </div>
     )
