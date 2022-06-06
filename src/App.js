c8e952c530a2cf5c0c0cf86b7acd1bf3a29cdc94
paras <parasrai@Jarvis.local>
2022-06-06 20:03:11 +0545

    Updated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import Navbar from './Component/1.Header/Navbar.js';
-import SearchArea from './Component/2.Body/SearchArea.js';
-import TopHeader from './Component/1.Header/TopHeader.js';
-import Category from './Component/2.Body/Category.js';
-import Slider from './Component/2.Body/Slider.js';
-import Footer from './Component/3.Footer/Footer.js';
+// import Navbar from './Component/1.Header/Navbar.js';
+// import SearchArea from './Component/2.Body/SearchArea.js';
+// import TopHeader from './Component/1.Header/TopHeader.js';
+// import Category from './Component/2.Body/Category.js';
+// import Slider from './Component/2.Body/Slider.js';
+// import Footer from './Component/3.Footer/Footer.js';
 import { Route, Routes, Link } from 'react-router-dom';
 import { HomePage } from './pages/home_page/home_page.js';
 import './assets/styles/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/footer.js';
+import { HomeSlider } from './components/home_slider.js';
+import { TopSection } from './components/top_section.js';
+import { NavigationBar } from './components/navbar.js';
+import { SearchBanner } from './components/banner.js';
+import { CategorySilder } from './pages/home_page/sections/category_slider.js';
 
 const App = () => {
 
   return (
     <>
-      <TopHeader />
-      <Navbar />
+      <TopSection />
+      <NavigationBar />
 
-      <SearchArea />
+      <SearchBanner />
 
-      <Category title="Categories" />
+      <CategorySilder title="Categories" />
 
-      <Slider title='Rental rooms' />
-      <Slider title='Apartments & Housing' />
-      <Slider title='Commercial & Business' />
-      <Slider title='Lands' />
-
+      <HomeSlider title='Rental rooms' />
+      <HomeSlider title='Apartments & Housing' />
+      <HomeSlider title='Commercial & Business' />
+      <HomeSlider title='Lands' />
       <br /><br /><br />
       <Footer />
 
