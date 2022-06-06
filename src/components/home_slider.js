f99073542df0bd922cdd46dd9f067ce799d09c5b
paras <parasrai@Jarvis.local>
2022-06-06 21:08:12 +0545

    on progress

--- src/components/home_slider.js
+++ src/components/home_slider.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 // import required modules
 import { Pagination, Navigation, Controller } from "swiper";
 import { Container, Row } from 'react-bootstrap';
