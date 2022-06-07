607e4bdec648c124480d61d5841c33172d2617e3
Bikash Rai <9819023310me@gmail.com>
2022-06-07 10:08:34 +0545

    rearranging files

--- src/index.js
+++ src/index.js
@@ -4,19 +4,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { DetailPage } from './pages/detail_page/detail_page';
-import { NotFoundPage } from './pages/not_found_page/not_found_page';
+import { DetailPage } from './Pages/detail_page/detail_page';
+import { NotFoundPage } from './Pages/not_found_page/not_found_page';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter >
-      <Routes>
-        < Route path="/" element={<App />} />
-        < Route path="about" element={<DetailPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
