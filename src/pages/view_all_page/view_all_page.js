55ebdd8d25fd07a78f2b41e23c8a1e3ed71e2c27
Bikash Rai <9819023310me@gmail.com>
2022-09-22 20:59:17 +0545

    view all page pagination

--- src/pages/view_all_page/view_all_page.js
+++ src/pages/view_all_page/view_all_page.js
@@ -1,29 +1,84 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import { RentalCard } from '../rental_card/rental_card';
 import './view_all_page.css'
 
 export default function ViewAll(props) {
-    let card_collection = []
-
-    for (let i = 0; i < 15; i++) {
-        card_collection.push(<RentalCard />)
+    //for grid view
+    let items = []
+    for (let i = 0; i < 30; i++) {
+        items.push(<RentalCard />)
     }
 
-    let title = props.title? props.title : 'No title';
+    function Items({ currentItems }) {
+        return (
+            <div className="container">
+                <div className="viewall-header">
+                    <span className='viewall-title'>{props.title ? props.title : 'No title'}</span>
+                    <div className="viewall-detail">
+                        Showing 100 properties
+                    </div>
+                </div>
+                <div style={{ borderBottom: '1px solid black', margin: '0.5rem 0' }}></div>
+                <div className="viewall-grid">
+                    {currentItems}
+                </div>
+            </div>
+        );
+    }
+
+    function PaginatedItems({ itemsPerPage }) {
+        const [currentItems, setCurrentItems] = useState(null);
+        const [pageCount, setPageCount] = useState(0);
+        const [itemOffset, setItemOffset] = useState(0);
+
+        useEffect(() => {
+            const endOffset = itemOffset + itemsPerPage;
+            setCurrentItems(items.slice(itemOffset, endOffset));
+            setPageCount(Math.ceil(items.length / itemsPerPage));
+        }, [itemOffset, itemsPerPage]);
+
+        const handlePageClick = (event) => {
+            const newOffset = (event.selected * itemsPerPage) % items.length;
+            setItemOffset(newOffset);
+        };
+
+        return (
+            <>
+                <Items currentItems={currentItems} />
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    renderOnZeroPageCount={null}
+
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    className='pagination'
+                />
+            </>
+        );
+    }
+
+
+
 
     return (
-        <div className="container">
-            <div className="viewall-header">
-                <span className='viewall-title'>{title}</span>
-                <div className="viewall-detail">
-                    Showing 100 properties
-                </div>
-
-            </div>
-            <div style={{borderBottom: '1px solid black', margin: '0.5rem 0'}}></div>
-            <div className="viewall-grid">
-                {card_collection}
-            </div>
-        </div>
+        <>
+            <PaginatedItems itemsPerPage={12} />
+        </>
     )
 }
